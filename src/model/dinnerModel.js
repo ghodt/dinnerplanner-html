217fea1ebc0c3cfc64e4d8753ed85627f88e0294
Sara Ersson <saraers@kth.se>
2019-09-03 13:34:38 +0200

    getDish works

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -2,18 +2,13 @@
 class DinnerModel {
 
   constructor() {
-    this.dishes = dishesConst;
-
-    //TODO Lab 0
-    // implement the data structure that will hold number of guests
-    // and selected dishes for the dinner menu
+  //  this.dishes = dishesConst;
     this.numberOfGuests = 0;
     this.dinnerMenu = [];
-
+    this.auth = { "X-Mashape-Key": key};
   }
 
   setNumberOfGuests(num) {
-    //TODO Lab 0
     // If num is negative numberOfGuests will not change
     if(num >= 0){
       this.numberOfGuests = num;
@@ -21,38 +16,33 @@ class DinnerModel {
   }
 
   getNumberOfGuests() {
-    //TODO Lab 0
     return this.numberOfGuests;
   }
 
   //Returns the dish that is on the menu for selected type
   getSelectedDish(type) {
-    //TODO Lab 0
+
     return this.dinnerMenu.find(dish => dish.type == type);
   }
 
   //Returns all the dishes on the menu.
   getFullMenu() {
-    //TODO Lab 0
     return this.dinnerMenu;
   }
 
   //Returns all ingredients for all the dishes on the menu.
   getAllIngredients() {
-    //TODO Lab 0
     return this.dinnerMenu.map((dish) => dish.ingredients).flat();
   }
 
   //Returns the total price of the menu (all the ingredients multiplied by number of guests).
   getTotalMenuPrice() {
-    //TODO Lab 0
     return this.numberOfGuests * this.getAllIngredients().reduce((acc, elem) => acc + elem.price, 0);
   }
 
   //Adds the passed dish to the menu. If the dish of that type already exists on the menu
   //it is removed from the menu and the new one added.
   addDishToMenu(id) {
-    //TODO Lab 0
     const newDish = this.getDish(id);
     const oldDish = this.getSelectedDish(newDish.type);
     if(oldDish !== undefined){
@@ -71,6 +61,7 @@ class DinnerModel {
   //Returns all dishes of specific type (i.e. "starter", "main dish" or "dessert").
   //query argument, text, if passed only returns dishes that contain the query in name or one of the ingredients.
   //if you don't pass any query, all the dishes will be returned
+  // FIX
   getAllDishes(type, query) {
     return this.dishes.filter(function (dish) {
       //If no type or query is specified
@@ -97,13 +88,13 @@ class DinnerModel {
   }
 
   //Returns a dish of specific ID
-  getDish(id) {
-    for (let dsh of this.dishes) {
-      if (dsh.id === id) {
-        return dsh;
-      }
-    }
-    return undefined;
+
+  async getDish(id) {
+    return fetch("http://sunset.nada.kth.se:8080/iprog/group/2/recipes/" + id + "/information", {
+      headers:  this.auth
+    })
+    .then(response => response.json())
+    .catch(console.error());
   }
 }
 
