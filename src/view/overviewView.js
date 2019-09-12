75343fedca7cbccfac714a32a9e884697b2b1504
Sara Ersson <saraers@kth.se>
2019-09-12 17:13:55 +0200

    vad

--- src/view/overviewView.js
+++ src/view/overviewView.js
@@ -6,24 +6,54 @@ class OverviewView {
 
     // An example of creating HTML procedurally. Think about the pros and cons of this approach.
     render() {
+      this.container.innerHTML = "";
       this.container.insertAdjacentHTML('beforebegin', header);
+      const div1 = this.container.appendChild(document.createElement('div'));
+      div1.id = "my-dinner-go-back";
       const paragraph = this.container.appendChild(document.createElement('P'))
-      paragraph.innerHTML = "This dinner will be Awesome!";
+    //  paragraph.className = "value-num-guests";
+      const num_of_guests = 3; // GET REAL DATA
+      paragraph.innerHTML = "My Dinner: " + num_of_guests + " people";
+      let goBackBtn = this.container.appendChild(document.createElement('button'));
+      goBackBtn.id = "go-back-btn";
+      goBackBtn.className = "btn btn-primary text-right"
+      goBackBtn.innerHTML = "Go back and edit dinner!";
+      div1.appendChild(paragraph);
+      div1.appendChild(goBackBtn);
 
-      const num_people_val = 3;
-      const paragraph2 = this.container.appendChild(document.createElement('P'))
-      const num_people = paragraph2.appendChild(document.createElement('SPAN'))
-      num_people.innerHTML = num_people_val;
-      paragraph2.innerHTML += " people are coming!";
+      const div2 = this.container.appendChild(document.createElement('div'))
+      div2.id = "my-dinner-overview";
 
-      const paragraph3 = this.container.appendChild(document.createElement('P'))
-      paragraph3.innerHTML = "We will be eating the following:";
+      for(const food of ["toast", "icecream"]) {
+        const dish = document.createElement('div');
+        dish.className = "dish";
+        const img_div = document.createElement('div');
+        let img = document.createElement('img');
+        img.src = "images/" + food + ".jpg";
+        const dish_title = document.createElement('p');
+        dish_title.className = ".value-main-course-name";
+        dish_title.innerHTML = "Icecream";
+        const price = document.createElement('p');
+      //  price.className = ""
+        price.innerHTML = "100 SEK";
 
-      const list = this.container.appendChild(document.createElement('UL'))
-
-      for(const food of ["Bread!", "Ham!", "Pizza!"]) {
-        list.appendChild(document.createElement('UL')).innerHTML = food;
+        img_div.appendChild(img);
+        img_div.appendChild(dish_title);
+        img_div.appendChild(price);
+        dish.appendChild(img_div);
+        div2.appendChild(dish);
       }
+      const totalPrice = div2.appendChild(document.createElement('p'));
+      const price = 300;
+      totalPrice.innerHTML = "Total: " + price + " SEK";
+
+      const div3 = this.container.appendChild(document.createElement('div'));
+      const printBtn = document.createElement('button');
+      printBtn.id = "toPrintBtn";
+      printBtn.className = "btn btn-primary";
+      printBtn.innerHTML = "Print Full Recipe";
+      div3.appendChild(printBtn);
+
 
       this.afterRender();
     }
